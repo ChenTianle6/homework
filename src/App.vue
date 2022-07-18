9301f303bdcf6b8ab42574771aa09d36805f40e1
ctl <3054008136@qq.com>
2022-07-18 13:52:14 +0800

    移动项目

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<style lang='scss'>
-body, html, #app {
-  height: 100%;
-}
-</style>
--- src/utils/Notification.js
+++
@@ -1,9 +0,0 @@
-import { ElNotification } from 'element-plus'
-
-export const Notification = (title, message, type) => {
-  ElNotification({
-    title,
-    message,
-    type
-  })
-}
